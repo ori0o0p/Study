a32d133a8e8e20a4e12f5540eb465df2ca83f808
ori0o0p <openwund8517@gmail.com>
2023-09-04 16:52:37 +0900

    add product entity

---
+++ src/main/java/com/example/shoppingmall/domain/product/entity/Product.java
@@ -0,0 +1,33 @@
+package com.example.shoppingmall.domain.product.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.validation.constraints.NotBlank;
+import java.util.Date;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@Document(collation = "product")
+public class Product {
+    @Id
+    private String id;
+
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String description;
+    @NotBlank
+    private Integer price;
+    @NotBlank
+    private Integer stock;
+    @NotBlank
+    private String category;
+    @NotBlank
+    private Date postdate;
+
+}
