caf036e870460aed9aefc3823b6670e2104ef674
ori0o0p <openwund8517@gmail.com>
2023-09-04 18:26:47 +0900

    add product response

--- src/main/java/com/example/shoppingmall/domain/product/controller/dto/response/ProductResponse.java
+++ src/main/java/com/example/shoppingmall/domain/product/controller/dto/response/ProductResponse.java
@@ -1,18 +1,19 @@
-package com.example.shoppingmall.domain.product.controller.dto.request;
+package com.example.shoppingmall.domain.product.controller.dto.response;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
+@Builder
 @RequiredArgsConstructor
-public class ProductRequest {
+public class ProductResponse {
     private String sellerEmail;
 
     private String title;
     private String description;
     private String category;
     private Integer price;
-    private Integer stock;
     private String image;
 
 }
