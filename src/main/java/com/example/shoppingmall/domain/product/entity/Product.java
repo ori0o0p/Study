21dd37d4c135c26a946e235a749e0b98bf20c786
ori0o0p <openwund8517@gmail.com>
2023-09-04 17:05:47 +0900

    edit product entity

--- src/main/java/com/example/shoppingmall/domain/product/entity/Product.java
+++ src/main/java/com/example/shoppingmall/domain/product/entity/Product.java
@@ -1,9 +1,12 @@
 package com.example.shoppingmall.domain.product.entity;
 
+import com.example.shoppingmall.domain.user.entity.User;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.validation.constraints.NotBlank;
@@ -11,22 +14,28 @@ import java.util.Date;
 
 @Getter
 @Setter
+@Builder
 @RequiredArgsConstructor
 @Document(collation = "product")
 public class Product {
     @Id
     private String id;
 
+    @DBRef
+    private User seller;
+
     @NotBlank
-    private String name;
+    private String title;
     @NotBlank
     private String description;
+    @NotBlank
+    private String category;
+
     @NotBlank
     private Integer price;
     @NotBlank
     private Integer stock;
-    @NotBlank
-    private String category;
+
     @NotBlank
     private Date postdate;
 }
