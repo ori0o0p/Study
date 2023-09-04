59bd15ee48c83ad37577fb6b76f3ab9fc7819123
ori0o0p <openwund8517@gmail.com>
2023-09-04 19:51:47 +0900

    edit

--- src/main/java/com/example/shoppingmall/domain/user/service/LoginService.java
+++ src/main/java/com/example/shoppingmall/domain/user/service/LoginService.java
@@ -19,7 +19,7 @@ public class LoginService {
     public LoginResponse execute(LoginRequest request) {
         User user = userRepository.findByEmail(request.getEmail())
                 .orElseThrow(() -> new RuntimeException()); // 예외 처리 예정
-        if (!passwordEncoder.matches(user.getPassword(), request.getPassword())) {
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
             throw new RuntimeException(); // 예외 처리 예정
         }
         String accessToken = jwtProvider.createAccessToken(request.getEmail());
