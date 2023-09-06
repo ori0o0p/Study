6c40dcb406d501de78c7ab5edde9a3eb81a27f17
ori0o0p <openwund8517@gmail.com>
2023-09-06 11:11:27 +0900

    add create review

--- src/main/java/com/example/shoppingmall/domain/review/service/ReviewCreateService.java
+++ src/main/java/com/example/shoppingmall/domain/review/service/ReviewCreateService.java
@@ -1,8 +1,14 @@
 package com.example.shoppingmall.domain.review.service;
 
+import com.example.shoppingmall.domain.product.entity.Product;
 import com.example.shoppingmall.domain.product.repository.ProductRepository;
+import com.example.shoppingmall.domain.product.service.facade.ProductFacade;
+import com.example.shoppingmall.domain.review.controller.dto.request.ReviewRequest;
+import com.example.shoppingmall.domain.review.entity.Review;
 import com.example.shoppingmall.domain.review.repository.ReviewRepository;
+import com.example.shoppingmall.domain.user.entity.User;
 import com.example.shoppingmall.domain.user.repository.UserRepository;
+import com.example.shoppingmall.domain.user.service.UserFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,13 +16,20 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ReviewCreateService {
     private final ReviewRepository reviewRepository;
-    private final ProductRepository productRepository;
-    private final UserRepository userRepository;
+    private final UserFacade userFacade;
+    private final ProductFacade productFacade;
 
-    public void execute() {
+    public void execute(ReviewRequest request) {
+        User user = userFacade.getUser();
+        Product product = productFacade.getProductById(request.getProductId());
 
+        reviewRepository.save(Review.builder()
+                .productId(product.getId())
+                .rating(request.getRating())
+                .contents(request.getContents())
+                .writerEmail(user.getEmail())
+                .imageURL(request.getImageURL())
+                .build());
     }
-
-
-
+    
 }
