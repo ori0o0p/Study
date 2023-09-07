3c76b50de07622259afd72f3334ef5d85a81c73c
ori0o0p <openwund8517@gmail.com>
2023-09-07 10:47:39 +0900

    edit

--- src/main/java/com/example/shoppingmall/domain/user/service/LoginService.java
+++ src/main/java/com/example/shoppingmall/domain/user/service/LoginService.java
@@ -4,7 +4,6 @@ import com.example.shoppingmall.domain.user.controller.dto.request.LoginRequest;
 import com.example.shoppingmall.domain.user.controller.dto.response.LoginResponse;
 import com.example.shoppingmall.domain.user.entity.User;
 import com.example.shoppingmall.domain.user.repository.UserRepository;
-import com.example.shoppingmall.domain.user.service.facade.UserFacade;
 import com.example.shoppingmall.global.security.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
