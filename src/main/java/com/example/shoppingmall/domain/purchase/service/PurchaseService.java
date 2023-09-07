fb1ed698f39dc34bf2014dd76ac080f5ade85a66
ori0o0p <openwund8517@gmail.com>
2023-09-07 19:13:42 +0900

    add facade

--- src/main/java/com/example/shoppingmall/domain/purchase/service/PurchaseService.java
+++ src/main/java/com/example/shoppingmall/domain/purchase/service/PurchaseService.java
@@ -1,8 +1,8 @@
 package com.example.shoppingmall.domain.purchase.service;
 
 import com.example.shoppingmall.domain.product.entity.Product;
-import com.example.shoppingmall.domain.product.repository.ProductRepository;
 import com.example.shoppingmall.domain.product.service.ProductGetStockService;
+import com.example.shoppingmall.domain.product.service.facade.ProductFacade;
 import com.example.shoppingmall.domain.purchase.controller.dto.request.PurchaseRequest;
 import com.example.shoppingmall.domain.purchase.controller.dto.response.PurchaseResponse;
 import lombok.RequiredArgsConstructor;
@@ -13,17 +13,16 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class PurchaseService {
-    private final ProductRepository productRepository;
     private final ProductGetStockService productGetStockService;
+    private final ProductFacade productFacade;
 
     public PurchaseResponse execute(PurchaseRequest request) {
-        Product product = productRepository.findById(request.getId())
-                .orElseThrow(() -> new RuntimeException()); //예외 처리 예정
+        Product product = productFacade.getProductById(request.getId());
 
         Integer stock = productGetStockService.execute(product.getId());
 
         if (stock < request.getAmount()) {
-            log.error("재고가 부족합니다.");
+            log.error("현재 재고 : " + stock + "재고가 부족합니다.");
             return PurchaseResponse.builder()
                     .stock(stock)
                     .isSuccess(false)
