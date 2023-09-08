ac2c48c9350a2d24c8638e9a45dd387bb443df29
ori0o0p <openwund8517@gmail.com>
2023-09-08 13:37:34 +0900

    edit

--- src/main/java/com/example/shoppingmall/global/security/JwtAuthFilter.java
+++ src/main/java/com/example/shoppingmall/global/security/JwtAuthFilter.java
@@ -25,7 +25,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
         String token = extractToken(request);
-        if(StringUtils.hasText(token) && jwtProvider.validateToken(token)){
+        if(jwtProvider.validateToken(token)){
             Authentication authentication = jwtProvider.getAuthentication(token);
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
