5fbbe4f552b5d7b0db241e8d474b897b5ca304f5
ori0o0p <openwund8517@gmail.com>
2023-09-11 09:17:53 +0900

    delete

--- src/main/java/com/example/shoppingmall/global/security/JwtProvider.java
+++ src/main/java/com/example/shoppingmall/global/security/JwtProvider.java
@@ -6,13 +6,11 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
