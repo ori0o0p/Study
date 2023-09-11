2b9d3985efbb723aca62974aa8a7a6d37d4ecde1
ori0o0p <openwund8517@gmail.com>
2023-09-11 15:46:44 +0900

    add service

---
+++ src/main/java/com/example/shoppingmall/domain/user/service/UserProductFindService.java
@@ -0,0 +1,41 @@
+package com.example.shoppingmall.domain.user.service;
+
+import com.example.shoppingmall.domain.product.controller.dto.response.ProductResponse;
+import com.example.shoppingmall.domain.product.entity.Product;
+import com.example.shoppingmall.domain.product.repository.ProductRepository;
+import com.example.shoppingmall.domain.review.controller.dto.response.ReviewResponse;
+import com.example.shoppingmall.domain.user.entity.User;
+import com.example.shoppingmall.domain.user.service.facade.UserFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserProductFindService {
+    private final UserFacade userFacade;
+    private final ProductRepository productRepository;
+
+    public List<ProductResponse> execute(String email) {
+        User user = userFacade.getUserByEmail(email);
+        List<Product> productList = productRepository.findBySellerId(user.getId());
+
+        return productList.stream().map(p -> new ProductResponse(
+                p.getSeller(),
+                p.getTitle(),
+                p.getDescription(),
+                p.getCategory(),
+                p.getPrice(),
+                p.getReview().stream().map(r -> new ReviewResponse(
+                        r.getRating(),
+                        r.getContent(),
+                        r.getCreatedDate(),
+                        r.getImageURL()
+                )).toList(),
+                p.getPostedDate(),
+                p.getImageURL()
+        )).toList();
+    }
+
+}
