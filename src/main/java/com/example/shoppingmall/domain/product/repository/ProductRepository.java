07d0a2fe73c7c96683f97217ed7ad1c263730432
ori0o0p <openwund8517@gmail.com>
2023-09-12 19:52:41 +0900

    add sort

--- src/main/java/com/example/shoppingmall/domain/product/repository/ProductRepository.java
+++ src/main/java/com/example/shoppingmall/domain/product/repository/ProductRepository.java
@@ -2,11 +2,14 @@ package com.example.shoppingmall.domain.product.repository;
 
 import com.example.shoppingmall.domain.product.entity.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
 public interface ProductRepository extends MongoRepository<Product, String> {
+    @Query(sort = "{postedDate: -1}")
     List<Product> findByCategory(String category);
+    @Query(sort = "{postedDate: -1}")
     List<Product> findBySellerId(String id);
 
 }
