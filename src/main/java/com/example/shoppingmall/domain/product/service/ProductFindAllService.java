f277bc659cd77ac5549b7913049fab348b4ef0a3
ori0o0p <openwund8517@gmail.com>
2023-09-14 20:07:08 +0900

    edit service

--- src/main/java/com/example/shoppingmall/domain/product/service/ProductFindAllService.java
+++ src/main/java/com/example/shoppingmall/domain/product/service/ProductFindAllService.java
@@ -2,6 +2,7 @@ package com.example.shoppingmall.domain.product.service;
 
 import com.example.shoppingmall.domain.product.controller.dto.response.ProductResponse;
 import com.example.shoppingmall.domain.product.entity.Product;
+import com.example.shoppingmall.domain.product.entity.ProductDetails;
 import com.example.shoppingmall.domain.product.repository.ProductRepository;
 import com.example.shoppingmall.domain.review.controller.dto.response.ReviewResponse;
 import lombok.RequiredArgsConstructor;
@@ -19,18 +20,19 @@ public class ProductFindAllService {
 
         return productList.stream().map(p -> new ProductResponse(
                 p.getSeller(),
-                p.getTitle(),
-                p.getDescription(),
-                p.getCategory(),
-                p.getPrice(),
+                new ProductDetails(
+                        p.getTitle(),
+                        p.getDescription(),
+                        p.getCategory(),
+                        p.getPrice(),
+                        p.getImageURL()),
                 p.getReview().stream().map(r -> new ReviewResponse(
                         r.getRating(),
                         r.getContent(),
                         r.getCreatedDate(),
                         r.getImageURL()
                 )).toList(),
-                p.getPostedDate(),
-                p.getImageURL()
+                p.getPostedDate()
         )).toList();
     }
 
